0cd0565fe7f60ffc6b7b36ff6937a5bfdc82311e
Minh Nguyen <nguyenhoangminhytcc@gmail.com>
2021-07-17 06:15:41 +0700

    final

--- B/app.js
+++ B/app.js
@@ -1,12 +1,26 @@
 
 async function getData(input) {
     let res = await fetch(`https://api.shrtco.de/v2/shorten?url=${input}`);
-    let data = JSON.parse(res);
+    let data = await res.json();
+
+    document.querySelector('small').innerHTML = 'Link generated!'
+    let result = document.querySelector('a');
+    if (document.getElementById("domain1").checked) {
+        result.href = data.result.full_short_link;
+        result.innerHTML = data.result.short_link;
+    } else if (document.getElementById("domain2").checked) {
+        result.href = data.result.full_short_link2;
+        result.innerHTML = data.result.short_link2;
+    } else if (document.getElementById("domain3").checked) {
+        result.href = data.result.full_short_link3;
+        result.innerHTML = data.result.short_link3;
+    }
 }
 
-function shortenLink(e) {
-    e.preventDefault();
-    let url = document.forms["myForm"]["fname"].value;
-    console.log(url)
+let submit = document.getElementById('submit');
 
-}
+submit.addEventListener('click', () => {
+    let link = document.getElementById('input').value;
+    getData(link);
+});
+
